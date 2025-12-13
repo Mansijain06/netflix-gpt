5d70351602a1550fde33e0b20aa84d1fd8ea72ef
Mansi Jain <mansijain061998@gmail.com>
2025-12-13 10:59:01 +0530

    added constant

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React, {useEffect} from "react";
-import { signOut } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
-import { addUser, removeUser } from "../utils/userSlice";
-import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-
-import { onAuthStateChanged } from "firebase/auth";
 import { useDispatch } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+import { LOGO } from "../utils/constant";
+import { USER_LOGO } from "../utils/constant";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -27,7 +28,7 @@ const Header = () => {
   };
 
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+    const unSubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         const { uid, email, displayName } = user;
         dispatch(addUser({ uid: uid, email: email, displayName: displayName }));
@@ -37,19 +38,20 @@ const Header = () => {
         navigate("/");
       }
     });
+    return () => unSubscribe();
   }, []);
   return (
     <div className="absolute w-screen bg-gradient-to-b from-black z-10 flex justify-between">
       <img
         className="w-44"
-        src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-12-03/consent/87b6a5c0-0104-4e96-a291-092c11350111/019ae4b5-d8fb-7693-90ba-7a61d24a8837/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        src={LOGO}
         alt="netflix logo"
       />
       {user && (
         <div className="flex gap-4 items-center m-4">
           <img
             className="w-10 h-10"
-            src="https://wallpapers.com/images/high/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.webp"
+            src={USER_LOGO}
             alt="user icon"
           />
           <button onClick={handleSignout} className="text-white font-bold">
